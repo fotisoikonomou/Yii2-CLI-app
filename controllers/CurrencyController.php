cf1f5215dbad2dc4fed82683fc1d3eb1d351abd9
fotisoikonomou <i_fotis@hotmail.com>
2024-03-15 03:38:22 +0200

    Update CurrencyController.php

--- controllers/CurrencyController.php
+++ controllers/CurrencyController.php
@@ -4,10 +4,6 @@ namespace app\commands;
 
 use Yii;
 
-use yii\console\ExitCode;
-use yii\httpclient\Client;
-use app\components\etl\implementations\CurrencyExtractor;
-
 
     public function actionIndex()
     {
